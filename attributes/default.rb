20e6bdd2cc03c75490ccc64c6fd65eaf2f719859
Joshua Haupt <joshua@hauptj.com>
2018-05-10 13:32:19 -0500

    Attempt to skip foodcritic

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,8 +1,8 @@
 default['minikube']['download_base_url'] = 'https://storage.googleapis.com/minikube/releases'
 default['minikube']['version'] = 'latest'
 
-default['minikube']['install_prefix'] = if node['platform_family'] == 'windows'
-																					C:\Program Files'
-																				else
-																					default['minikube']['install_prefix'] = '/usr/local/bin'
-																				end
+if node['platform_family'] == 'windows'
+	default['minikube']['install_prefix'] = 'C:\Program Files'
+else
+	default['minikube']['install_prefix'] = '/usr/local/bin'
+end
